07f30c2154c255ea20e9793376436305f6d98a4b
Maarten Hermans <maarten@herwig-ubuntu1.(none)>
2009-04-23 14:51:15 +0200

    testfuncties verzelfstandigd, mapping.py importeerbaar

--- utils/mapping.py
+++ utils/mapping.py
@@ -38,46 +38,63 @@ class Article(rdfSubject):
 #class Journal(Periodical):
 #    rdf_type = BIBO.Journal
 
-graph = rdfSubject.db = ConjunctiveGraph()
-graph.parse('/home/maarten/workdir/serialservice/data/capitalclass97.ttl', format="n3")
-
-
 mapper()
 
-a = Article()
-for article in a.ClassInstances():
-    if article.title != None:
-        print article.title
-        print '\t', 'By', ' and '.join(article.creators)
-        #for c in article.creators:
-        #    print "\t", "Author;", c
-        print '\t', article.summary[0:30], '...'
-        print '\t', 'Pages', '-'.join([str(article.sPg), str(article.ePg)])
-        print '\t', article.issue.periodical.title, 'nummer', article.issue.number
-        print '\t', ''.join(['ISSN: ', article.issue.periodical.issn, '.']), 'Uitgever:', article.issue.periodical.publisher
-        print '\n'
 
-j = Periodical.get_by(issn="0309-8168")
-print j.title
 
-from genshi.template import MarkupTemplate
-t = """
-<html xmlns:py="http://genshi.edgewall.org/">
-  <head>
-    <title py:content="journal.title">This is replaced.</title>
-  </head>
-  <body>
-    <p>Table of contents:</p>
-    <ul>
-      <li py:for="article in articles">
-        Title: ${article.title}s
-      </li>
-    </ul>
-  </body>
-</html>
-"""
+def testOutput():
+    graph = rdfSubject.db = ConjunctiveGraph()
+    graph.parse('/home/maarten/workdir/serialservice/data/capitalclass97.ttl', format="n3")
 
-tmpl = MarkupTemplate('<h1>Hello, $journal.title</h1>')
-tmpl = MarkupTemplate(t)
-stream = tmpl.generate(journal=j, articles=a.ClassInstances())
-#print stream.render('xhtml')
+    a = Article()
+    for article in a.ClassInstances():
+        if article.title != None:
+            print article.title
+            print '\t', 'By', ' and '.join(article.creators)
+            #for c in article.creators:
+            #    print "\t", "Author;", c
+            print '\t', article.summary[0:30], '...'
+            print '\t', 'Pages', '-'.join([str(article.sPg), str(article.ePg)])
+            print '\t', article.issue.periodical.title, 'nummer', article.issue.number
+            print '\t', ''.join(['ISSN: ', article.issue.periodical.issn, '.']), 'Uitgever:', article.issue.periodical.publisher
+            print '\n'
+
+    j = Periodical.get_by(issn="0309-8168")
+    print j.title
+
+def testTemplate():
+
+
+    graph = rdfSubject.db = ConjunctiveGraph()
+    graph.parse('/home/maarten/workdir/serialservice/data/capitalclass97.ttl', format="n3")
+    j = Periodical.get_by(issn="0309-8168")
+    a = Article()
+
+    from genshi.template import MarkupTemplate
+    t = """
+    <html xmlns:py="http://genshi.edgewall.org/">
+    <head>
+        <title py:content="journal.title">This is replaced.</title>
+    </head>
+    <body>
+        <p>Table of contents:</p>
+        <ul>
+            <li py:for="article in articles">
+                Title: ${article.title}s
+            </li>
+        </ul>
+    </body>
+    </html>
+    """
+
+    tmpl = MarkupTemplate('<h1>Hello, $journal.title</h1>')
+    tmpl = MarkupTemplate(t)
+    stream = tmpl.generate(journal=j, articles=a.ClassInstances())
+    print stream.render('xhtml')
+
+
+
+if __name__ == "__main__":
+    testOutput()
+    testTemplate()
+    #pass
