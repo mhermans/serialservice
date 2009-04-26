6a6978b0469332c2e326a539e75102a892842ed7
Maarten Hermans <maarten@herwig-ubuntu1.(none)>
2009-04-26 12:51:41 +0200

    unittests uitgebreid, datadump/voorbeelddata Sampol

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,10 @@
+#! /usr/bin/env pyton
+# -*- coding: utf-8 -*-
+import unittest, sys
 
-import unittest
-import sys
 sys.path.append('../utils')
-
 from mapping import Article, Issue, Periodical
+
 from rdfalchemy import rdfSubject 
 from rdfalchemy.orm import mapper
 from rdflib import ConjunctiveGraph
@@ -11,7 +12,7 @@ from rdflib import ConjunctiveGraph
 class TestBasicMapping(unittest.TestCase):
 
     def setUp(self):
-        self.graph = ConjunctiveGraph = rdfSubject.db
+        self.graph = rdfSubject.db
 
     def testMapAssertions(self):
         self.expectedMapping = {
@@ -23,24 +24,45 @@ class TestBasicMapping(unittest.TestCase):
         self.assertEqual(self.expectedMapping, mapper())
 
     def testUnitMapping(self):
-        a = Article
-        a.sPg = 3
-
+        self.assertEqual(0, len(self.graph))
+        a = Article()
+        self.assertEqual(1, len(self.graph)) # bnode--rdf:type-->bibo:article
+        a.title = "Testtitle"
+        self.assertEqual(2, len(self.graph)) # bnode--rdf:type-->bibo:article
+        #self.assertEqual(0, list(self.graph.triples((None, None, None))))
+    
+    def testInitiationAndAssignment(self):
+        a = Article()
 
     def tearDown(self):
-        self.graph = None
+        self.graph.remove((None, None, None))
+        self.assertEqual(0, len(self.graph))
+
+class TestSomething(unittest.TestCase):
+    
+    def setUp(self):
+        self.graph = rdfSubject.db
+
+    def testUriInitialization(self):
+        self.assertEqual(0, len(self.graph))
+        a = Article("<tag:test-article,2009-04-25>")
+        self.assertEqual(0, len(self.graph)) #XXX bug? waarom geen rdf:type bibo:Article (bnode initialisatie wél)
+    
+    def tearDown(self):
+        self.graph.remove((None, None, None))
+        self.assertEqual(0, len(self.graph))
 
 class TestLoadDataMapping(unittest.TestCase):
 
     def setUp(self):
-        self.graph = ConjunctiveGraph = rdfSubject.db
+        self.graph = rdfSubject.db
 
     def testLoad(self):
         self.graph.parse("/home/maarten/workdir/serialservice/data/capitalclass97.ttl", format="n3")
         self.assertEqual(85, len(self.graph))
 
     def tearDown(self):
-        self.graph = None
+        self.graph.remove((None, None, None))
 
 if __name__ == "__main__":
     unittest.main()   
