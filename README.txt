ce41648c62a5fab5533d142bbf52e8d724e39f6b
Maarten Hermans <maarten@herwig-ubuntu1.(none)>
2009-04-29 22:25:33 +0200

    start Pylons project

---
+++ README.txt
@@ -0,0 +1,19 @@
+This file is for you to describe the serialservice application. Typically
+you would include information such as the information below:
+
+Installation and Setup
+======================
+
+Install ``serialservice`` using easy_install::
+
+    easy_install serialservice
+
+Make a config file as follows::
+
+    paster make-config serialservice config.ini
+
+Tweak the config file as appropriate and then setup the application::
+
+    paster setup-app config.ini
+
+Then you are ready to go.
---
+++ serialservice/config/environment.py
@@ -0,0 +1,34 @@
+"""Pylons environment configuration"""
+import os
+
+from genshi.template import TemplateLoader
+from pylons import config
+
+import serialservice.lib.app_globals as app_globals
+import serialservice.lib.helpers
+from serialservice.config.routing import make_map
+
+def load_environment(global_conf, app_conf):
+    """Configure the Pylons environment via the ``pylons.config``
+    object
+    """
+    # Pylons paths
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    paths = dict(root=root,
+                 controllers=os.path.join(root, 'controllers'),
+                 static_files=os.path.join(root, 'public'),
+                 templates=[os.path.join(root, 'templates')])
+
+    # Initialize config with the basic options
+    config.init_app(global_conf, app_conf, package='serialservice', paths=paths)
+
+    config['routes.map'] = make_map()
+    config['pylons.app_globals'] = app_globals.Globals()
+    config['pylons.h'] = serialservice.lib.helpers
+
+    # Create the Genshi TemplateLoader
+    config['pylons.app_globals'].genshi_loader = TemplateLoader(
+        paths['templates'], auto_reload=True)
+
+    # CONFIGURATION OPTIONS HERE (note: all config options will override
+    # any Pylons config options)
